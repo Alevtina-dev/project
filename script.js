57c9d24b8cc1f0b8dd1e1da15bc0543539456839
Alevtina <zaripova-alevtina@rambler.ru>
2019-12-04 23:46:30 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+'use strict';
+//console.log(typeof(null));
+
+
+//console.log("arr" + " - object");
+
+//console.log(4 + " - object");
+
+let answer = prompt("Есть ли Вам 18?", "Да");
+
+//console.log(answer);
+
+let incr = 10,
+decr = 10;
+
+
+
+//console.log(incr++);
+//console.log(decr--);
+
+let isChecked = true,
+isClose = false;
+
+console.log(isChecked || isClose);
