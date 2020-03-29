e22988a5ce8926db2f7a7ebc30bab5463b57ca04
Alevtina <zaripova-alevtina@rambler.ru>
2020-03-29 22:48:22 +0400

    commit 29 march

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 'use strict';
 
-
-let money = prompt("Ваш бюджет на месяц?",""), 
-    time = prompt("Введите дату в формате YYYY-MM-DD","");
+let money = +prompt("Ваш бюджет на месяц?", '');
+let time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 let appData = {
     budget: money,
@@ -13,9 +12,49 @@ let appData = {
     savings: false
 };
 
-let a1 = prompt("Введите обязательную статью расходов в этом месяце",""),
-a2 = prompt("Во сколько обойдется?","");
 
-appData.expenses.a1 = a2;
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце",""),
+        b = prompt("Во сколько обойдется?","");
+    if( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b))=== 'string' && a != '' && b != '' && a.length <50 ) {
+        console.log("done");
+        appData.expenses[a] = b;
+    } else {
+        console.log("bad result");
+        i--;
+    }
+}
+/*let i=0;
+while (i < 2) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце",""),
+    b = prompt("Во сколько обойдется?","");
+    if( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b))=== 'string' && a != '' && b != '' && a.length <50 ) {
+        console.log("done");
+        appData.expenses[a] = b;
+    }
+    i++;
+}
 
-alert(appData.budget/30);
+do {
+    let a = prompt("Введите обязательную статью расходов в этом месяце",""),
+    b = prompt("Во сколько обойдется?","");
+    if( (typeof(a))=== 'string' && (typeof(a)) != null && (typeof(b))=== 'string' && a != '' && b != '' && a.length <50 ) {
+        console.log("done");
+        appData.expenses[a] = b;
+    }
+    i++;
+} while (i<2);*/
+
+appData.moneyPerDay = appData.budget/30;
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay <100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка"); 
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Средний уровень достатка");
+} else {
+    console.log("Ошибка");
+}
